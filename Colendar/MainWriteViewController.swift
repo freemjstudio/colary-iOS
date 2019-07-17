c47de3c74231010084230ed77e2bf9143c8089ef
freemjstudio <mjwoo0@naver.com>
2019-07-18 00:51:23 +0900

    .

--- Colendar/MainWriteViewController.swift
+++ Colendar/MainWriteViewController.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2019 우민지. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MainWriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
