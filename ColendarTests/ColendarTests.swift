b234a5b01b21a3051575d270eb7aca789fe5b2b1
freemjstudio <mjwoo0@naver.com>
2019-07-18 00:02:40 +0900

    storyboard

---
+++ ColendarTests/ColendarTests.swift
@@ -0,0 +1,34 @@
+//
+//  ColendarTests.swift
+//  ColendarTests
+//
+//  Created by 우민지 on 13/07/2019.
+//  Copyright © 2019 우민지. All rights reserved.
+//
+
+import XCTest
+@testable import Colendar
+
+class ColendarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
